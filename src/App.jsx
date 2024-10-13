985c66cd8d808cbd518d46106e2650f136a6195c
Parinaz <parinaz.kapourchian@gmail.com>
2024-10-13 21:05:01 +0330

    Design Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header, Home, About, Products } from "./pages";
 function App() {
-
   return (
-   <div>
-    
-   </div>
-  )
+    <div>
+      <BrowserRouter>
+      <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
